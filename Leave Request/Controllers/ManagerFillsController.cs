a64816f632c52e2192bad9cb4291a66d819e5fd6
Octa Alief Kiemas Sahid <octa.aks@gmail.com>
2021-10-01 16:20:50 +0700

    Controllers

---
+++ Leave Request/Controllers/ManagerFillsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NETCore.Base;
+using Leave_Request.Models;
+using Leave_Request.Repositories.Data;
+
+namespace NETCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManagerFillsController : BaseController<ManagerFill, ManagerFillRepository, int>
+    {
+        public ManagerFillsController(ManagerFillRepository repository) : base(repository)
+        {
+
+        }
+    }
+}
