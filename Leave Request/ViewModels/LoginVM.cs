3679ec130a408290352b6efd66393f7f425e1989
Febryani Sri Waranti <febryaniswr@gmail.com>
2021-10-04 11:39:47 +0700

    edit loginvm

--- Leave Request/ViewModels/LoginVM.cs
+++ Leave Request/ViewModels/LoginVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,8 @@ namespace Leave_Request.ViewModels
 {
     public class LoginVM
     {
+        [Required(ErrorMessage = "")]
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
