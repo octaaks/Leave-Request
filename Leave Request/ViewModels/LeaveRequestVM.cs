3e3931f2f86ee62ca15f0eea44eeda5f8171124f
Octa Alief Kiemas Sahid <octa.aks@gmail.com>
2021-10-04 13:43:22 +0700

    add view models (leave req, change password)

---
+++ Leave Request/ViewModels/LeaveRequestVM.cs
@@ -0,0 +1,21 @@
+﻿using Leave_Request.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Leave_Request.ViewModels
+{
+    public class LeaveRequestVM
+    {
+        [Key]
+        public int Id { get; set; }
+        public int LeaveTypeId { get; set; }
+        public DateTime RequestDate { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int LeaveDuration { get; set; }
+        public int EmployeeId { get; set; }
+        public int StatusId { get; set; }
+    }
+}
