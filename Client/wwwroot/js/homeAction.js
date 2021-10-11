b4d63cc089cf34e6296bb7443d43ef87bc67e556
Octa Alief Kiemas Sahid <octa.aks@gmail.com>
2021-10-11 14:01:13 +0700

    register form validation

--- Client/wwwroot/js/homeAction.js
+++ Client/wwwroot/js/homeAction.js
@@ -1,20 +1,99 @@
 ﻿//styling
 document.getElementById("titleform").style.fontFamily = 'Tahoma, sans-serif';
 
-//posting data registration form to db
-$("#btnSubmit").click(post => {
-    var obj = DataInsert();
-    console.log(obj);
-    post.preventDefault();
+(function () {
+    'use strict';
+    window.addEventListener('load', function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Something went wrong!',
+                    })
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === true) {
+                    event.preventDefault();
+
+                    var obj = {
+
+                        //"Id": 12,
+                        //"Name": "Cumi Cumi",
+                        //"Address": "Bandung, Indonesia",
+                        //"Gender": 1,
+                        //"Phone": "0812322126007",
+                        //"BirthDate": "1994-05-11",
+                        //"Salary": 1000000,
+                        //"JoinDate": "2020-10-05",
+                        //"ReligionId": 1,
+                        //"JobId": 1,
+                        //"Email": "ciciici7@gmail.com",
+                        //"Password": "Qwerty007"
+
+                        "Id": parseInt($('#inputId').val()),
+                        "Name": $('#inputName').val(),
+                        "Address": $('#inputAddress').val(),
+                        "Gender": parseInt($('#inputGender').val()),
+                        "Phone": $('#inputPhone').val(),
+                        "BirthDate": moment($('#inputBirthDate').val()).format("YYYY-MM-DD"),
+                        "Salary": parseInt($('#inputSalary').val()),
+                        "JoinDate": moment($('#inputJoinDate').val()).format("YYYY-MM-DD"),
+                        "ReligionId": parseInt($('#inputReligion').val()),
+                        "JobId": parseInt($('#inputJob').val()),
+                        "Email": $('#inputEmail').val(),
+                        "Password": $('#inputPassword').val()
+
+                        //"Id": parseInt($('#inputId').val()),
+                        //"Name": $('#inputName').val(),
+                        //"Address": $('#inputAddress').val(),
+                        //"Gender": parseInt($('#inputGender').val()),
+                        //"Phone": $('#inputPhone').val(),
+                        //"BirthDate": $('#inputBirthDate').val(),
+                        //"Salary": parseInt($('#inputSalary').val()),
+                        //"JoinDate": $('#inputJoinDate').val(),
+                        //"ReligionId": parseInt($('#inputReligion').val()),
+                        //"JobId": parseInt($('#inputJob').val()),
+                        //"Email": $('#inputEmail').val(),
+                        //"Password": $('#inputPassword').val()
+                    };
+
+                    var data = JSON.stringify(obj);
+
+                    register(data);
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
+
+function register(inputdata) {
+    console.log(inputdata);
+
     $.ajax({
         url: "/Accounts/Register",
         type: 'POST',
-        data: data,
+        data: inputdata,
         traditional: true,
         contentType: 'application/json',
         dataType: "json",
         success: function (data, x, xhr) {
-            if (data.statusCode == 200) {
+
+            console.log(data.status);
+
+            //if (data.status == 200) {
                 Swal.fire({
                     title: "Welcome!",
                     text: "Register successful. You can Login now",
@@ -22,21 +101,21 @@ $("#btnSubmit").click(post => {
                     button: "OK"
                 });
                 Reset();
-                $('#insertRegis').modal('hide')
-            }
-            else {
-                Swal.fire({
-                    title: "Oh no...",
-                    text: "Register unsuccessful ...",
-                    icon: "error",
-                    button: "OK"
-                });
-            }
+                $('#insertRegis').modal('hide');
+                $('.modal-backdrop').remove();
+            //}
+            //else {
+            //    Swal.fire({
+            //        title: "Oh no...",
+            //        text: "Register unsuccessful ...",
+            //        icon: "error",
+            //        button: "OK"
+            //    });
+            //}
             console.log(data);
-            /* $('#insertData').modal('hide');*/
         },
         fail: function (data) {
-            Swal.fire({
+            Swal.fire({ 
                 title: "Oh no...",
                 text: "Register unsuccessful ...",
                 icon: "error",
@@ -57,32 +136,28 @@ $("#btnSubmit").click(post => {
     });
 };
 
+//posting data registration form to db
 //$("#btnSubmit").click(post => {
-
-//    debugger;
-
 //    var obj = DataInsert();
 //    console.log(obj);
-//    var email = $('#email').val();
-//    console.log(email);
 //    post.preventDefault();
-
 //    $.ajax({
-//        url: "/Accounts/Registering",
+//        url: "/Accounts/Register",
 //        type: 'POST',
-//        data: obj,
-//        dataType: 'json',
+//        data: data,
+//        traditional: true,
+//        contentType: 'application/json',
+//        dataType: "json",
 //        success: function (data, x, xhr) {
 //            if (data.statusCode == 200) {
-//                //$('#insertRegis').modal('hide')
 //                Swal.fire({
 //                    title: "Welcome!",
 //                    text: "Register successful. You can Login now",
 //                    icon: "success",
 //                    button: "OK"
 //                });
-//               // $('#dataTable').DataTable().ajax.reload();
 //                Reset();
+//                $('#insertRegis').modal('hide')
 //            }
 //            else {
 //                Swal.fire({
@@ -112,10 +187,10 @@ $("#btnSubmit").click(post => {
 //                button: "OK"
 //            });
 //            console.log(error);
-//            Reset();
+//            //Reset();
 //        }
 //    });
-//})
+//};
 
 //get data value registration form
 function DataInsert() {
