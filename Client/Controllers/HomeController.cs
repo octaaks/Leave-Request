6a3ffbdd5351d920d431cf613ebceebc5b9e0f72
Febryani Sri Waranti <febryaniswr@gmail.com>
2021-10-16 15:23:54 +0700

    Modify Datatable Data Employee

--- Client/Controllers/HomeController.cs
+++ Client/Controllers/HomeController.cs
@@ -85,10 +85,10 @@ namespace Client.Controllers
                 return RedirectToAction("Index", "Home");
             }
         }
-        [Authorize(Roles = "Approver")]
+        
         public IActionResult ManageLeaveRequest()
         {
-            if (User.Identity.IsAuthenticated)
+            if (User.Identity.IsAuthenticated && User.IsInRole("Approver"))
             {
                 ViewBag.Id = HttpContext.Session.GetString("Id");
                 return View();
@@ -98,12 +98,13 @@ namespace Client.Controllers
                 return RedirectToAction("Index", "Home");
             }
         }
-
-        [Authorize(Roles = "Administrator")]
+       
         public IActionResult Employees()
         {
             if (User.Identity.IsAuthenticated && User.IsInRole("Administrator"))
             {
+                ViewBag.Id = HttpContext.Session.GetString("Id");
+
                 return View();
             }
             else
